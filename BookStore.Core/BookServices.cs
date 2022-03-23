cd38b04f0c2f9706262de9112383a65f99741ec0
YehyaShahbur <mohamad.yehya.shahbur@gmail.com>
2022-03-23 22:34:41 +0200

    Add project files.

---
+++ BookStore.Core/BookServices.cs
@@ -0,0 +1,44 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+
+namespace BookStore.Core
+{
+    public class BookServices : IBookServices
+    {
+        private readonly IMongoCollection<Book> _books;
+
+        public BookServices(IDbClient dbClient)
+        {
+            _books = dbClient.GetBooksCollection();
+        }
+
+        public Book AddBook(Book book)
+        {
+            _books.InsertOne(book);
+            return book;
+        }
+
+        public void DeleteBook(string id)
+        {
+            _books.DeleteOne(x => x.Id == id);
+        }
+
+        public Book GetBook(string id)
+        {
+            return _books.Find(x => x.Id == id).First();
+        }
+
+        public List<Book> GetBooks()
+        {
+            return _books.Find(book => true).ToList();
+        }
+
+        public Book UpdateBook(Book book)
+        {
+            GetBook(book.Id);
+            _books.ReplaceOne(x => x.Id == book.Id, book);
+            return book;
+        }
+    }
+}
---
+++ BookStore.Core/DbClient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Core
+{
+    public class DbClient : IDbClient
+    {
+        private readonly IMongoCollection<Book> _books;
+        public DbClient(IOptions<BookStoreDbConfig> bookstoreDbconfig)
+        {
+            var client = new MongoClient(bookstoreDbconfig.Value.Connection_String);
+            var database = client.GetDatabase(bookstoreDbconfig.Value.Database_Name);
+            _books = database.GetCollection<Book>(bookstoreDbconfig.Value.Book_Collection_Name);
+
+        }
+        public IMongoCollection<Book> GetBooksCollection()
+        {
+            return _books;
+        }
+    }
+}
